93a5e936fe3011fd22dea0e3e843d5770ddb225e
kksn9611 <kksn9611@gmail.com>
2026-03-10 13:10:21 +0900

    Minor

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -8,12 +8,17 @@ public class TestScript : MonoBehaviour
         // HexGridLayout이 타일을 다 생성할 때까지 대기
         yield return new WaitForSeconds(0.2f);
 
-        // 시간이 지났으니 이제 딕셔너리에 값이 꽉 차 있습니다!
-        TileScript targetTile = TileManager.Instance.GetTile(new Vector2Int(0, 0));
-
+        TileScript spawnTile = TileManager.Instance.GetTile(new Vector2Int(0, 0));
+        TileScript targetTile = TileManager.Instance.GetTile(new Vector2Int(5, 5));
+        if (spawnTile != null)
+        {
+            UnitSpawner.Instance.SpawnUnit(mobUnit, spawnTile);
+            Debug.Log("spawnTile에 유닛 소환");
+        }
         if (targetTile != null)
         {
             UnitSpawner.Instance.SpawnUnit(mobUnit, targetTile);
+            Debug.Log("targetTile에 유닛 소환");
         }
     }
 
